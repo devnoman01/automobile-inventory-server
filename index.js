eaa2c791eb0dde4b9eb5ad5f7436c7207cd97298
devnoman01 <devnoman01@gmail.com>
2022-05-08 20:35:10 +0600

    data load from server complete

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
+require("dotenv").config();
 
 // use middleware
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5xcel.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const vehicleCollection = client
+      .db("automobileInventory")
+      .collection("vehicle");
+
+    // LOAD all vehicle API
+    app.get("/vehicle", async (req, res) => {
+      const query = {};
+      const cursor = vehicleCollection.find(query);
+      const vehicles = await cursor.toArray();
+      res.send(vehicles);
+    });
+
+    // LOAD specific vehicle
+    app.get("/vehicle/:vehicleId", async (req, res) => {
+      const id = req.params.vehicleId;
+      const query = { _id: ObjectId(id) };
+      const vehicle = await vehicleCollection.findOne(query);
+      res.send(vehicle);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
-  res.send("Running Latest CRUD Server");
+  res.send("Running Automobile Inventory CRUD Server");
 });
 
 app.listen(port, () => {
-  console.log("Latest CRUD server is running");
+  console.log("Automobile Inventory server is running");
 });
